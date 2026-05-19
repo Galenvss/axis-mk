e606c993978d4dff271a040a65878681629db2a0
Galenvss <103313407+Galenvss@users.noreply.github.com>
2026-05-19 23:44:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Şu an için ekstra bir JavaScript aksiyonuna gerek yok.
+// CSS'teki 'scroll-behavior: smooth;' işimizi görüyor.
+// İleride buraya G-Wolf logosuna tıklayınca uluma sesi vs. ekleriz, sen şimdilik boş bırakma bunu yapıştır.
+console.log("AXIS MK Karargah Sistemi Aktif.");
